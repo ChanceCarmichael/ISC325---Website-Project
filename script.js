b314d1a7d5dbdada9cbc5adab8ae96a659b8506b
Chance Carmichael <horeth@oswego.edu>
2019-02-05 20:56:36 -0500

    Added basic script

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#upperLeft').click(function(){
+        $('#upLDescription').toggle();
+        //$('#upLPicture').toggle();
+    });
+    
+    $('#upperRight').click(function(){
+        $('#upRDescription').toggle();
+        //$('#upRPicture').toggle();
+    })
+})
